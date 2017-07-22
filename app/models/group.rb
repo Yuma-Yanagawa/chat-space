f65d337c10e5e9b46cf3bc570041ba95094c0ba6
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-07-22 22:26:20 +0900

    Complete message transmission

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,14 @@ class Group < ApplicationRecord
   has_many :users, through: :groups_users
   has_many :messages
   has_many :groups_users
+
+  def puts_messages
+    if messages.last.try(:image).present?
+      '画像が投稿されています'
+    elsif messages.last.try(:body)
+      messages.last.try(:body)
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
