e9bf6a6fdcfee082bfe4f19af84a2a84b0a1885d
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-07-22 22:23:45 +0900

    I can send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,5 +7,20 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @message = Message.new(message_params)
+    @messages = @group.messages
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージを送信しました"
+    else
+      flash.now[:alert] = "メッセージを入力してください"
+      render :index
+    end
   end
+
+   private
+   def message_params
+     params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+   end
 end
