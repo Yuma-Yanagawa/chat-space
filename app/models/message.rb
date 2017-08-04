046767fc1afc674b21a9573fd4c28c936b4373bc
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 19:53:50 +0900

    edit validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,12 +3,4 @@ class Message < ApplicationRecord
   belongs_to :group
   mount_uploader :image, ImageUploader
   validates :body, presence: true, unless: :image?
-
-  def message_cannnot_be_in_the_past
-    if body.present? || image.present?
-      return true
-    else
-      return false
-    end
-  end
 end
