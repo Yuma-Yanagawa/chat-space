890b368e06803a70bb9df1c2cb42953f246f3533
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 16:35:33 +0900

    edit include_text

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -26,19 +26,19 @@ describe Message do
       it "is invalid without a body, image" do
         message = build(:message, body: nil, image: nil)
         message.valid?
-        expect(message).not_to be_valid
+        expect(message.errors[:body]).to include('を入力してください')
       end
 
       it "is invalid without a group_id" do
         message = build(:message, group_id: nil)
         message.valid?
-        expect(message).not_to be_valid
+        expect(message.errors[:group]).to include('を入力してください')
       end
 
       it "is invalid without a user_id" do
         message = build(:message, user_id: nil)
         message.valid?
-        expect(message).not_to be_valid
+        expect(message.errors[:user]).to include('を入力してください')
       end
     end
   end
