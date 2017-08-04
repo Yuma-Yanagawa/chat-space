b8c70cf15daf20aee85f9caf46e8d7401a9680ee
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 19:46:20 +0900

    edit user_id and group_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
-  validates :body, presence: true, unless: :message_cannnot_be_in_the_past
+  validates :body, presence: true, unless: :image?
 
   def message_cannnot_be_in_the_past
     if body.present? || image.present?
