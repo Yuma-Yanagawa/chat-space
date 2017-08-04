b97e5e4706bbf7c36f29eb97ff5287bce52548e8
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 18:58:29 +0900

    add after(:create) do |user|

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,9 @@ FactoryGirl.define do
     email              { Faker::Internet.email }
     password           { Faker::Number.digit }
     name               { Faker::Name.name }
+
+    after(:create) do |user|
+      create(:group_user, user: user, group: create(:group))
+    end
   end
 end
