4a449ec667a6790011d81916b672d5607093b29d
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 15:45:37 +0900

    Add context

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,41 +2,44 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
+    context 'valid' do
+      it "is valid with a body" do
+        message = build(:message, image: nil)
+        message.valid?
+        expect(message).to be_valid
+      end
 
-    it "is valid with a body" do
-      message = build(:message, image: nil)
-      message.valid?
-      expect(message).to be_valid
+      it "is valid with a image" do
+        message = build(:message, body: nil)
+        message.valid?
+        expect(message).to be_valid
+      end
+
+      it "is valid with a body, image" do
+        message = build(:message)
+        message.valid?
+        expect(message).to be_valid
+      end
     end
 
-    it "is valid with a image" do
-      message = build(:message, body: nil)
-      message.valid?
-      expect(message).to be_valid
-    end
+    context 'invalid' do
+      it "is invalid without a body, image" do
+        message = build(:message, body: nil, image: nil)
+        message.valid?
+        expect(message).not_to be_valid
+      end
 
-    it "is valid with a body, image" do
-      message = build(:message)
-      message.valid?
-      expect(message).to be_valid
-    end
+      it "is invalid without a group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message).not_to be_valid
+      end
 
-    it "is invalid without a body, image" do
-      message = build(:message, body: nil, image: nil)
-      message.valid?
-      expect(message).not_to be_valid
-    end
-
-    it "is invalid without a group_id" do
-      message = build(:message, group_id: nil)
-      message.valid?
-      expect(message).not_to be_valid
-    end
-
-    it "is invalid without a user_id" do
-      message = build(:message, user_id: nil)
-      message.valid?
-      expect(message).not_to be_valid
+      it "is invalid without a user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message).not_to be_valid
+      end
     end
   end
 
