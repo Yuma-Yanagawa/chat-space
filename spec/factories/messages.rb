a5a5c796e394a5aeacb1b71c0688152fae5c24d7
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 09:17:33 +0900

    pass message by Faker::Internet.email

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,17 +2,10 @@ FactoryGirl.define do
 
   factory :message do
     id         1
-    body       "test_message1"
+    body       { Faker::Internet.email }
     image      { Rack::Test::UploadedFile.new Rails.root.join('spec/fixtures/image.jpg'), 'image/jpg' }
     group_id   1
     user_id    1
-    # created_at {Faker::Time.between(2.days.ago, Time.now, :all)}
-    # updated_at {Faker::Time.between(2.days.ago, Time.now, :all)}
-
-    # after(:build) do |message|
-    #   message.user << FactoryGirl.build(:user, id: message.user_id)
-    #   message.group << FactoryGirl.build(:cgroup, id: message.group_id)
-    # end
   end
 
 end
