3e622c00112a4d9ab1691eceb14b50d48cccdbf5
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 18:32:34 +0900

    change body to Faker::Lorem.word

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :message do
-    body       "test_message1"
+    body       { Faker::Lorem.word }
     image      { Rack::Test::UploadedFile.new Rails.root.join('spec/fixtures/image.jpg'), 'image/jpg' }
     group_id   1
     user_id    1
