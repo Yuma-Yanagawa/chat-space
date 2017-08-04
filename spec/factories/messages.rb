660bcdb6e9c14c8072b6abd606b51c90cd2cad4b
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-04 19:46:30 +0900

    edit user_id and group_id

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     body       { Faker::Lorem.word }
     image      { Rack::Test::UploadedFile.new Rails.root.join('spec/fixtures/image.jpg'), 'image/jpg' }
     group_id   1
-    user_id    1
+    user_id    1 
   end
 
 end
