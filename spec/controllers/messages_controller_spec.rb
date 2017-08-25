bdeae1078ab651fcc056a4055e4c42635d6ecb4f
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-25 15:20:31 +0900

    pass all tests

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -14,6 +14,11 @@ describe MessagesController, type: :controller do
           get :index, group_id: group.id
           expect(assigns(:group)).to eq group
         end
+
+        it "renders the :index template" do
+          get :index, group_id: group.id
+          expect(response).to render_template :index
+        end
     end
 
     context "when user don't loged in" do
@@ -30,23 +35,31 @@ describe MessagesController, type: :controller do
         login_user user
       end
       it "assigns the requested message to @message" do
-        expect{
-    post :create, params: { message: attributes_for(:message, { body: 'hello' }), group_id: group.id }
-  }.to change(Message, :count).by(1)
+        expect{ post :create, params: { message: attributes_for(:message, { body: 'hello' }), group_id: group.id } }.to change(Message, :count).by(1)
+      end
+
+      it "redirect_to group_messages_path" do
+        post :create, params: { message: attributes_for(:message, { body: 'hello' }), group_id: group.id }
+        expect(response).to redirect_to group_messages_path
+      end
+    end
+
+    context "when user loged in but also message isn't saved" do
+      it "assigns a newly created but unsaved message as @message" do
+        expect{ post :create, params: { message: attributes_for(:message, { body: 'hello' }), group_id: group.id } }.not_to change(Message, :count)
+      end
+
+      it 'renders the :index template' do
+         post :create, params: { message: attributes_for(:message, { body: 'hello' }), group_id: group.id }
+         expect(response).to redirect_to new_user_session_path
+       end
+    end
+
+    context "when user don't loged in" do
+      it "redirect_to index screen" do
+        post :create, params: { message: attributes_for(:message, { body: 'hello' }), group_id: group.id }
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
 end
-
-# describe 'GET #index' do
-#     it "populates an array of tweets ordered by created_at DESC" do
-#       tweets = create_list(:tweet, 3)
-#       get :index
-#       expect(assigns(:tweets)).to match(tweets.sort{|a, b| b.created_at <=> a.created_at })
-#     end
-#
-#     it "renders the :index template" do
-#       get :index
-#       expect(response).to render_template :index
-#     end
-#   end
