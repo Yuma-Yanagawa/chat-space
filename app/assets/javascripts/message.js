6311364cb941f355653d166b9a439928795d5218
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-08-30 15:33:01 +0900

    write method of done

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,17 @@
 $(function() {
+  function buildHTML(message){
+    var html = `<div class="main__chat__messages__body-user-name">
+                 ${message.user.name}
+                </div>
+                <div class="main__chat__messages__body-date">
+                ${message.created_at.strftime("%Y/%m/%d %H:%M")}
+                </div>
+                <p class="main__chat__messages__body-content">
+                ${message.image.to_s}
+                ${message.body}
+                </p>
+                </div>`
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +23,12 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data){
+        $('.main__chat__messages__body-content').append(html)
+        $('.footer__chat__form__js-form__message').val('')
+      }
     })
   });
 });
