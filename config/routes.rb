4c5a5273460b29401e9d812e3b4ff57ebc447db0
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-09-02 14:17:47 +0900

    To set up rooting

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
