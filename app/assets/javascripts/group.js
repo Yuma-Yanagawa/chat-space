1b4a317fe98ce064eb89fbe4c27c3415adf57b51
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-09-05 12:55:34 +0900

    implement incremenatal_search

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,17 +3,26 @@ function appendUser(user){
   var html = `<li class="chat-group-user">
                 <div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name${user.name}>追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>
-              </li>`
-  $("#chat-group-users").append(html);
+              </li>`;
+  $("#user-search-result").append(html);
 }
 
 function appendNoUser(user) {
     var html = `<li class="chat-group-user">
                   <div class='listview__element--right-icon'>${ user }</div>
-                </li>`
-    $("#chat-group-users").append(html);
+                </li>`;
+    $("#user-search-result").append(html);
+  }
+
+  function buildMember(id, name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    return html;
   }
 
   $(".chat-group-form__input").on("keyup", function() {
@@ -30,7 +39,6 @@ function appendNoUser(user) {
         $("li").remove();
         data.forEach(function(user){
          appendUser(user);
-         console.log(user);
        });
      }
      else {
@@ -41,5 +49,17 @@ function appendNoUser(user) {
    .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  });
+  $(".chat-group-form").on('click', ".user-search-add", function() {
+      var id = $(this).data("user-id");
+      var name = $(this).data("user-name");
+      var html = buildMember(id, name);
+      $('#chat-group-users').append(html);
+      var user = $(this).parent();
+      user.remove();
+  });
+  $(".chat-group-form").on('click', ".user-search-remove", function() {
+      var user = $(this).parent();
+      user.remove();
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users_sp = User.where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
