79e6cb239db9d42199c87679bf2a0cf3a2598b2a
Yuma_Yanagawa <yumayanagawa.3106@gmail.com>
2017-09-07 21:35:32 +0900

    complete6 deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.115.28.16', user: 'ec2-user', roles: %w{app db web}
+server '13.115.28.164', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
